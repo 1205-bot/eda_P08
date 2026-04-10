f4920fd3eee6be5055d3abae31e52abc50d4abf5
unknown <ypollo20@gmail.com>
2026-04-09 20:08:41 -0600

    Subo nodo.c

--- src/nodo.c
+++ src/nodo.c
@@ -1,12 +1,11 @@
 #include "nodo.h"
+#include <string.h>
 
 Nodo *crear_nodo(void *info, size_t size) {
     Nodo *n = (Nodo*)malloc(sizeof(Nodo));
-    if(!n)return;
-
+    if(!n)return NULL;
     n->info = malloc(size);
-    if(!n->info) { free(n); return }
-
+    if(!n->info) { free(n); return NULL; }
     memcpy(n->info, info, size);
     n->ant = n->sig = NULL;
     return n;
@@ -18,13 +17,14 @@ void liberar_nodo(Nodo *n) {
     } else {
         free(n->info);
         free(n);
-        n = NULL;
     }
 }
 
-bool actualizar_nodo(void *info, size_t size) {
-    nodo->info = realloc(nodo->info, size);
-    if(!nodo->info) return false;
-    memcpy(nodo->info, info, size);
+bool actualizar_nodo(Nodo *n, void *info, size_t size) {
+    if(!n) return false;
+    void *tmp = realloc(n->info, size);
+    if(!tmp) return false;
+    n->info = tmp;
+    memcpy(n->info, info, size);
     return true;
 }
